2e44fa74a31e85efe0a809c07a315ebfc69cbe39
NOUREENTAZ <85793985+NOUREENTAZ@users.noreply.github.com>
2023-06-26 13:49:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,15 +51,20 @@ child2.display();
 
 Array.prototype.sum = function() {
     let result = 0;
-    let N;
-    for (let i = 0; i <= N; i++) {
+    for (let i = 0; i < this.length; i++) {
         result += this[i];
     }
     return result;
 };
 
-let N1 = [7, 8, 7];
-let N2 = [8, 4, 2];
+let N1 = [4, 20, 5, 8];
+let N2 = [7, 5, 9, 1, 8, 5, 6];
+
+let sum1 = N1.sum();
+let sum2 = N2.sum();
+
+console.log(sum1);
+console.log(sum2);
 
 
 let addition = N1.sum() + N2.sum()
@@ -68,26 +73,26 @@ console.log(addition);
 
 //Q4
 
-// class student {
-//     constructor(name, rollno, division){
-//         this.name= name;
-//         this.rollno= rollno;
-//         this.division = division; 
-//     }
+class student {
+    constructor(name, rollno, division) {
+        this.name = name;
+        this.rollno = rollno;
+        this.division = division;
+    }
 
-//     set setFriend(friend){
-//         this.friend= friend
-//     }
-// }
+    set setFriend(friend) {
+        this.friend = friend;
+    }
+}
 
-// let obj = new student("Noureentaz, "50","A")
-// obj.friend="Nagaveni"
-// console.log(obj)
+let obj = new student("Noureentaz", 50, "A");
+obj.friend = "Nagaveni"
+console.log(obj)
 
-// class senior extends student{
-//      constructor(){
-//         super("Sujeen", "49", "B")
-//      }
-// }
-//  let seniorobj = new senior();
-//  console.log(seniorobj)
+class senior extends student {
+    constructor() {
+        super("Sujeen", 49, "B")
+    }
+}
+let seniorobj = new senior();
+console.log(seniorobj)
